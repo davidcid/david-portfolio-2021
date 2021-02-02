e5772dcea7a2ff71acbbcfddb89329ab99d965ad
David Cid <david.cid@gmail.com>
2021-02-02 18:00:36 +0100

    set colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import "../stylesheets/footer.scss";
+import { FaEnvelope, FaTwitter, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
 
 const Footer = () => {
     return ( 
         <div className="footer" id="footer">
-            <h2>Este es el footer!</h2>
+            <div className="container">
+                <ul className="footer-social">
+                    <li className="footer-social-element"><a href="https://www.linkedin.com/in/david-cid-lendinez/" target="_blank" rel="noreferrer"><FaLinkedinIn /></a></li>
+                    <li className="footer-social-element"><a href="https://github.com/davidcid" target="_blank" rel="noreferrer"><FaGithub /></a></li>
+                    <li className="footer-social-element"><a href="https://twitter.com/DavidCidDev" target="_blank" rel="noreferrer"><FaTwitter /></a></li>
+                    <li className="footer-social-element"><a href="mailto:david.cid@gmail.com" target="_blank" rel="noreferrer"><FaEnvelope /></a></li>
+                </ul>
+                <p className="footer-p">Made with ❤️ and ⚛️ by David Cid</p>
+            </div>
+            
         </div>
      );
 }
