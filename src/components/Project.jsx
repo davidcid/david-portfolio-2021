fe9288cc28c32d9ce27227fae0db7199968671d2
David Cid <david.cid@gmail.com>
2021-02-04 13:32:25 +0100

    fixed css issue

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,5 @@
 import { Carousel } from "react-responsive-carousel";
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import "../stylesheets/project.scss";
 
@@ -21,153 +21,136 @@ import sonospineTreatment from "../assets/images/sonospine/treatment.jpg";
 import sonospineStories from "../assets/images/sonospine/stories.jpg";
 
 // iconos tecnologias
-import { FaReact, FaNodeJs, FaAngular } from 'react-icons/fa';
-import { SiJavascript, SiMysql, SiTwilio, SiTypescript } from 'react-icons/si';
+import { FaReact, FaNodeJs, FaAngular } from "react-icons/fa";
+import { SiJavascript, SiMysql, SiTwilio, SiTypescript } from "react-icons/si";
 
-
-import { BiLink } from 'react-icons/bi';
-
+import { BiLink } from "react-icons/bi";
 
 const projects = [
-    {
-        name: "amphosfarmacia",
-        title: "Amphosfarmacia",
-        description: "Projecto que creé desde cero utilizando React.js en el front y Node.js para el backend. Utilizado por profesionales de la salud para compartir sus experiencias con el COVID-19 y comunicarse con otros profesionales.",
-        url: "https://www.amphosfarmacia.com",
-        images:
-            [{
-                image: amphosfarmaciaLanding,
-                alt: "Amphosfarmacia landing page"
-            }, 
-            {
-                image: amphosfarmaciaInbox,
-                alt: "Amphosfarmacia bandeja de entrada"
-            },
-            {
-                image: amphosfarmaciaParticipate,
-                alt: "Amphosfarmacia como participar"
-            },
-            {
-                image: amphosfarmaciaVerify,
-                alt: "Amphosfarmacia verificacion de perfiles"
-            }],
-        technologies:
-            [
-                <FaReact />,
-                <FaNodeJs />,
-                <SiMysql />
-            ]
-    },
-    {
-        name: "careexpand",
-        title: "Careexpand",
-        description: "Participé en el desarrollo de la aplicación de Careexpand orientada al registro de datos de pacientes y comunicación paciente/doctor. Entre otras tecnologías, utilicé Twilio para las video conferencias y Stripe para procesar los pagos.",
-        url: "https://www.careexpand.com",
-        images: 
-            [{
-                image: careexpandLanding,
-                alt: "Careexpand landing page"
-            }, 
-            {
-                image: careexpandCareCoordination,
-                alt: "Careexpand care coordination page"
-            },
-            {
-                image: careexpandAdminDashboard,
-                alt: "Dashboard de Careexpand"
-            },
-            {
-                image: careexpandTelemedicine,
-                alt: "Pagina de telemedicina de Careexpand"
-            },
-        ],
-        technologies:
-        [
-            <SiJavascript />,
-            <FaReact />,
-            <FaNodeJs />,
-            <SiTwilio />,
-            <SiMysql />
-        ]
-    },
-    {
-        name: "sonospine",
-        title: "Sonospine",
-        description: "Colaboré en el mantenimiento de la parte privada de esta herramienta utilizada para hacer seguimiento a pacientes con afecciones de espalda.",
-        url: "https://sonospinesurgery.com/",
-        images: 
-            [{
-                image: sonospineLanding,
-                alt: "Sonospine landing page"
-            }, 
-            {
-                image: sonospineTreatment,
-                alt: "Sonospine treatment page"
-            },
-            {
-                image: sonospineStories,
-                alt: "Sonospine stories page"
-            },
-        ],
-        technologies:
-        [
-            <FaAngular />,
-            <SiTypescript />
-        ]
-    },
+  {
+    name: "amphosfarmacia",
+    title: "Amphosfarmacia",
+    description:
+      "Projecto que creé desde cero utilizando React.js en el front y Node.js para el backend. Utilizado por profesionales de la salud para compartir sus experiencias con el COVID-19 y comunicarse con otros profesionales.",
+    url: "https://www.amphosfarmacia.com",
+    images: [
+      {
+        image: amphosfarmaciaLanding,
+        alt: "Amphosfarmacia landing page",
+      },
+      {
+        image: amphosfarmaciaInbox,
+        alt: "Amphosfarmacia bandeja de entrada",
+      },
+      {
+        image: amphosfarmaciaParticipate,
+        alt: "Amphosfarmacia como participar",
+      },
+      {
+        image: amphosfarmaciaVerify,
+        alt: "Amphosfarmacia verificacion de perfiles",
+      },
+    ],
+    technologies: [<FaReact />, <FaNodeJs />, <SiMysql />],
+  },
+  {
+    name: "careexpand",
+    title: "Careexpand",
+    description:
+      "Participé en el desarrollo de la aplicación de Careexpand orientada al registro de datos de pacientes y comunicación paciente/doctor. Entre otras tecnologías, utilicé Twilio para las video conferencias y Stripe para procesar los pagos.",
+    url: "https://www.careexpand.com",
+    images: [
+      {
+        image: careexpandLanding,
+        alt: "Careexpand landing page",
+      },
+      {
+        image: careexpandCareCoordination,
+        alt: "Careexpand care coordination page",
+      },
+      {
+        image: careexpandAdminDashboard,
+        alt: "Dashboard de Careexpand",
+      },
+      {
+        image: careexpandTelemedicine,
+        alt: "Pagina de telemedicina de Careexpand",
+      },
+    ],
+    technologies: [
+      <SiJavascript />,
+      <FaReact />,
+      <FaNodeJs />,
+      <SiTwilio />,
+      <SiMysql />,
+    ],
+  },
+  {
+    name: "sonospine",
+    title: "Sonospine",
+    description:
+      "Colaboré en el mantenimiento de la parte privada de esta herramienta utilizada para hacer seguimiento a pacientes con afecciones de espalda.",
+    url: "https://sonospinesurgery.com/",
+    images: [
+      {
+        image: sonospineLanding,
+        alt: "Sonospine landing page",
+      },
+      {
+        image: sonospineTreatment,
+        alt: "Sonospine treatment page",
+      },
+      {
+        image: sonospineStories,
+        alt: "Sonospine stories page",
+      },
+    ],
+    technologies: [<FaAngular />, <SiTypescript />],
+  },
 ];
 
 const Project = ({ project }) => {
+  const selectedProject = projects.filter((item) => item.name === project)[0];
 
-    const selectedProject = projects.filter(item => item.name === project)[0];
-
-    return ( 
-        <div className="project">
-            <h2 className="project-title">{selectedProject.title}</h2>
-            <Carousel
-                showArrows
-                showStatus={false}
-                autoPlay
-                interval={4500}
-                infiniteLoop={true}
-                showThumbs={false}
-                className="project-carousel"
+  return (
+    <div className="project">
+      <div className="container project-container">
+        <div className="legend">
+          <h3 className="legend-title">
+            <a
+              className="legend-link"
+              href={selectedProject.url}
+              target="_blank"
+              rel="noreferrer"
             >
-                {
-                    selectedProject.images.map(image => 
-                        (
-                            <div className="project-carousel-div">
-                                <img src={image.image} alt={image.alt}/>
-                                <div className="legend">
-                                    <h3 className="legend-title">
-                                        {selectedProject.title}
-                                        <a 
-                                            className="legend-link" 
-                                            href={selectedProject.url}
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            <BiLink />
-                                        </a>
-                                    </h3>
-                                    <p className="legend-text">{selectedProject.description}</p>
-                                    <ul className="legend-techs">
-                                    {
-                                        selectedProject.technologies.map(tech => 
-                                            (
-                                                <li className="legend-tech">{tech}</li>
-                                            )
-                                        )
-                                    }
-                                    </ul>
-                                </div>
-                            </div>
-                        )
-                    )
-                }
-            </Carousel>
+              {selectedProject.title} <BiLink />
+            </a>
+          </h3>
+          <p className="legend-text">{selectedProject.description}</p>
+          <ul className="legend-techs">
+            {selectedProject.technologies.map((tech) => (
+              <li className="legend-tech">{tech}</li>
+            ))}
+          </ul>
+        </div>
+        <Carousel
+          showArrows
+          showStatus={false}
+          autoPlay
+          interval={4500}
+          infiniteLoop={true}
+          showThumbs={false}
+          className="project-carousel"
+        >
+          {selectedProject.images.map((image) => (
+            <div className="project-carousel-div">
+              <img src={image.image} alt={image.alt} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </div>
-     );
-}
- 
+  );
+};
+
 export default Project;
